25575110b5fcfb59da30cfcaf6019592e4af931f
unknown <unknown@example.com>
2024-01-07 18:50:24 +0800

    rm

--- cl2.ps1
+++
@@ -1,6 +0,0 @@
-dl-tool.exe zhiapi-ques  570729170   -r100000000
-dl-tool.exe zhiapi-ques  582979328   -r100000000
-dl-tool.exe zhiapi-ques  582326598   -r100000000
-dl-tool.exe zhiapi-ques  581556221   -r100000000
-dl-tool.exe zhiapi-ques  570765297   -r100000000
-dl-tool.exe zhiapi-ques  584515782   -r100000000
